1f4c1a7e22964a55d5b3dae2b5c4a6770c3739bb
Taylor Miller <Aylr@users.noreply.github.com>
2021-07-15 15:55:50 -0600

    * Big improvements
    * brew install some useful tools
    * neovim setup with plugins and themes
    * mypy config archive - not installed yet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,14 @@
 
 ## [Unreleased]
 
+### Added
+
+* neovim install, plugins, and configs
+* brew install script
+* A nice clean neovim setup including plugins and themes.
+* Default git branch name uses more inclusive language
+* lots of new aliases for git, docker, and more
+
 ## [0.2.0] - 2020-05-017-06-20
 
 ### Added
--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# install oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Set up symlinks
-ln -s bash/.bash_profile ~/.bash_profile
-ln -s bash/.gitconfig ~/.gitconfig
+ln -s "$(pwd)"/zsh/.zshrc ~/.zshrc
+ln -s "$(pwd)"/bash/.bash_profile ~/.bash_profile
+ln -s "$(pwd)"/bash/.gitconfig ~/.gitconfig
+
+
+#######################
+# Neovim
+#######################
+
+# Config symlinks
+mkdir ~/.config/nvim || echo 'nvim config directory exists'
+mkdir ~/.config/nvim/plugged || echo 'nvim plugged directory exists'
+
+# Install plugged https://github.com/junegunn/vim-plug
+# TODO I'm not sure if this is required or does autoload fix it?
+# curl -fLo ~/.config/nvim/plugged/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s "$(pwd)/nvim/autoload" ~/.config/nvim/autoload
+ln -s "$(pwd)/nvim/init.vim" ~/.config/nvim/init.vim
+
+# Plugins. Run twice so themes are all set up.
+nvim --headless +PlugInstall +qa
+nvim --headless +PlugInstall +qa
+
